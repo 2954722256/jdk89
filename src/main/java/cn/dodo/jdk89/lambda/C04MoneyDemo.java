4ecef81e5709c8629692ffbefa2f2e61573e2faf
dodo_lihao <2390183798@qq.com>
2018-05-16 22:45:51 +0800

    add

--- src/main/java/cn/dodo/jdk89/lambda/C04MoneyDemo.java
+++ src/main/java/cn/dodo/jdk89/lambda/C04MoneyDemo.java
@@ -3,10 +3,38 @@ package cn.dodo.jdk89.lambda;
 import java.text.DecimalFormat;
 import java.util.function.Function;
 
-class MyMoney {
+/**
+ * 第1种写法
+ *      定义接口 （是函数式接口， 只有一个 未实现方法）
+ *      传入 int， 返回 String
+ */
+interface IMoneyFormat{
+    String format(int i);
+}
+
+/**
+ * 第1种写法
+ *      传入接口实现即可
+ */
+class MyMoney1 {
     private final int money;
 
-    public MyMoney(int money) {
+    public MyMoney1(int money) {
+        this.money = money;
+    }
+
+    public void printMoney(IMoneyFormat moneyFormat) {
+        System.out.println("我的存款：" + moneyFormat.format(this.money));
+    }
+}
+
+/**
+ * 函数式
+ */
+class MyMoney2 {
+    private final int money;
+
+    public MyMoney2(int money) {
         this.money = money;
     }
 
@@ -22,13 +50,32 @@ class MyMoney {
 public class C04MoneyDemo {
 
     public static void main(String[] args) {
-        MyMoney me = new MyMoney(99999999);
+        do01();
+        do02();
+    }
+
+    private static void do02() {
+        MyMoney2 me = new MyMoney2(99999999);
 
         Function<Integer, String> moneyFormat = i -> new DecimalFormat("#,###")
                 .format(i);
 
         // 函数接口链式操作
         me.printMoney(moneyFormat.andThen(s -> "人民币 " + s));
+    }
+
+
+    private static void do01() {
+        MyMoney1 me = new MyMoney1(99999999);
+        // 接口的写法
+        me.printMoney(i -> new DecimalFormat("#,###").format(i));
+
+//        me.printMoney(new IMoneyFormat(){
+//            @Override
+//            public String format(int i) {
+//                return new DecimalFormat("#,###").format(i);
+//            }
+//        });
     }
 
 }

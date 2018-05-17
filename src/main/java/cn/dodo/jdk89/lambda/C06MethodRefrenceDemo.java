b22c8cd91263fce54d48ee4d49970e2682b047a4
dodo_lihao李灏 <2954722256@qq.com>
2018-05-17 23:12:19 +0800

    add

--- src/main/java/cn/dodo/jdk89/lambda/C06MethodRefrenceDemo.java
+++ src/main/java/cn/dodo/jdk89/lambda/C06MethodRefrenceDemo.java
@@ -8,29 +8,22 @@ import java.util.function.IntUnaryOperator;
 public class C06MethodRefrenceDemo {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat(3);
 
-        // 方法引用
-//        Consumer<String> consumer = s -> System.out.println(s);
-        Consumer<String> consumer = System.out::println;
-        consumer.accept("接受的数据");
+        do01LambdaSimple();
 
-        // 静态方法的方法引用
-        Consumer<Dog> consumer2 = Dog::bark;
-        consumer2.accept(dog);
+        Dog dog = do02DogLambdaStatic();
 
         // 非静态方法，使用对象实例的方法引用
-        // Function<Integer, Integer> function = dog::eat;
-        // UnaryOperator<Integer> function = dog::eat;
-        IntUnaryOperator function = dog::eat;
+        // Function<Integer, Integer> function = dog::eat;  //一般的函数式写法
+        // UnaryOperator<Integer> function = dog::eat;  //输入-输出，一样， 用 UnaryOperator<XXX>
+        IntUnaryOperator function = dog::eat;   //输入-输出，一样都是Int， 用 IntUnaryOperator
 
         // dog置空，不影响下面的函数执行，因为java 参数是传值
         dog = null;
         System.out.println("还剩下" + function.applyAsInt(2) + "斤");
         //
         // // 使用类名来方法引用
-        // BiFunction<Dog, Integer, Integer> eatFunction = Dog::eat;
+        // BiFunction<Dog, Integer, Integer> eatFunction = Dog::eat;    //BiFunction， 有2个参数
         // System.out.println("还剩下" + eatFunction.apply(dog, 2) + "斤");
         //
         // // 构造函数的方法引用
@@ -47,6 +40,27 @@ public class C06MethodRefrenceDemo {
 
         System.err.println(list);
     }
+
+    /**
+     *  静态方法的引用  （引用是 静态方法，  方法对象.accept(对象)）
+     * @return
+     */
+    private static Dog do02DogLambdaStatic() {
+        Dog dog = new Dog();
+        dog.eat(3);
+        // 静态方法的方法引用
+        Consumer<Dog> consumer2 = Dog::bark;
+        consumer2.accept(dog);
+        return dog;
+    }
+
+    private static void do01LambdaSimple() {
+        // 方法引用
+//        Consumer<String> consumer = s -> System.out.println(s);
+        Consumer<String> consumer = System.out::println;
+        consumer.accept("接受的数据");
+    }
+
     private static void test00(List<String> list) {
         list = null;
     }
